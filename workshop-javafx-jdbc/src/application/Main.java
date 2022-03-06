6898fb8e98bd32447d809772339f241272361e09
Marta Schmitt <mcristina.schmitt@gmail.com>
2022-03-06 17:08:00 -0300

    About view

--- workshop-javafx-jdbc/src/application/Main.java
+++ workshop-javafx-jdbc/src/application/Main.java
@@ -8,6 +8,9 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
+	
+	private static Scene mainScene;
+	
 	@Override
 	public void start(Stage primaryStage) {
 		try {
@@ -17,7 +20,7 @@ public class Main extends Application {
 			scrollPane.setFitToHeight(true);
 			scrollPane.setFitToWidth(true);
 			
-			Scene mainScene = new Scene(scrollPane);
+			mainScene = new Scene(scrollPane);
 			primaryStage.setScene(mainScene);
 			primaryStage.setTitle("Sample JavaFX application");
 			primaryStage.show();
@@ -26,7 +29,12 @@ public class Main extends Application {
 		}
 	}
 	
+	public static Scene getMainScene() {
+		return mainScene;
+	}
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
+		
 }
